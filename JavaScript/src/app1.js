94e4ae2bc4313dd08d5080a14374355ff1232d06
tanmay-37 <talekartk@rknec.edu>
2024-02-02 18:54:40 +0530

    Added javaScript file

---
+++ JavaScript/src/app1.js
@@ -0,0 +1,22 @@
+console.log("Hello, World! Again");
+
+var name = "Tanmay";
+
+let age = 19;
+
+const city = "Nagpur";
+
+console.log(name);
+
+const myObject = {
+    name: "Tanmay",
+    age: 19,
+    city: "Nagpur",
+    state: "MH",
+};
+
+console.log(myObject.name);
+console.log(myObject);
+
+const label = `This is sample ${myObject.name}`;
+console.log(label);
